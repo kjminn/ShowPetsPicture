f68efdadfbb7cd84c70855da5823a7e8582ec976
kjminn <kjminn2001@gmail.com>
2016-10-28 18:11:28 +0900

    #2 change

--- app/src/main/java/kr/ac/hywoman/minn/showpetspicture/MainActivity.java
+++ app/src/main/java/kr/ac/hywoman/minn/showpetspicture/MainActivity.java
@@ -4,35 +4,36 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Switch;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    CheckBox checkStart;
+    Switch switchStart;
     TextView textQ2;
     RadioGroup rg;
     RadioButton rbPuppy, rbCat, rbRabbit;
-    Button butDone;
+    Button butFinish, butFirst;
     ImageView imgPet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        checkStart=(CheckBox)findViewById(R.id.check_start);
+        switchStart=(Switch) findViewById(R.id.switch_start);
         textQ2=(TextView) findViewById(R.id.text_q2);
         rg=(RadioGroup) findViewById(R.id.rg);
         rbPuppy=(RadioButton) findViewById(R.id.rb_puppy);
         rbCat=(RadioButton) findViewById(R.id.rb_cat);
         rbRabbit=(RadioButton) findViewById(R.id.rb_rabbit);
-        butDone=(Button) findViewById(R.id.but_done);
+        butFinish=(Button) findViewById(R.id.but_finish);
+        butFirst=(Button) findViewById(R.id.but_first);
         imgPet=(ImageView) findViewById(R.id.img_pet);
 
-        checkStart.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        switchStart.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if(isChecked){
@@ -41,35 +42,56 @@ public class MainActivity extends AppCompatActivity {
                     rbPuppy.setVisibility(View.VISIBLE);
                     rbCat.setVisibility(View.VISIBLE);
                     rbRabbit.setVisibility(View.VISIBLE);
-                    butDone.setVisibility(View.VISIBLE);
                     imgPet.setVisibility(View.VISIBLE);
+                    butFinish.setVisibility(View.VISIBLE);
+                    butFirst.setVisibility(View.VISIBLE);
                 }else{
                     //보이는 뷰들을 안보이게 설정
                     textQ2.setVisibility(View.INVISIBLE);
                     rbPuppy.setVisibility(View.INVISIBLE);
                     rbCat.setVisibility(View.INVISIBLE);
                     rbRabbit.setVisibility(View.INVISIBLE);
-                    butDone.setVisibility(View.INVISIBLE);
-                    imgPet.setVisibility(View.INVISIBLE);                    
+                    imgPet.setVisibility(View.INVISIBLE);
+                    butFinish.setVisibility(View.INVISIBLE);
+                    butFirst.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        butDone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                switch (rg.getCheckedRadioButtonId()){
-                    case R.id.rb_puppy:
-                        imgPet.setImageResource(R.drawable.puppy);
-                        break;
-                    case R.id.rb_cat:
-                        imgPet.setImageResource(R.drawable.cat);
-                        break;
-                    case R.id.rb_rabbit:
-                        imgPet.setImageResource(R.drawable.rabbit);
-                        break;
-                }
-            }
-        });
+        rbPuppy.setOnClickListener(rbHandler);
+        rbCat.setOnClickListener(rbHandler);
+        rbRabbit.setOnClickListener(rbHandler);
+
+        butFinish.setOnClickListener(butHandler);
+        butFirst.setOnClickListener(butHandler);
     }
+    View.OnClickListener rbHandler=new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.rb_puppy:
+                    imgPet.setImageResource(R.drawable.puppy);
+                    break;
+                case R.id.rb_cat:
+                    imgPet.setImageResource(R.drawable.cat);
+                    break;
+                case R.id.rb_rabbit:
+                    imgPet.setImageResource(R.drawable.rabbit);
+                    break;
+            }
+        }
+    };
+    View.OnClickListener butHandler= new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.but_finish:
+                    finish();
+                    break;
+                case R.id.but_first:
+                    switchStart.setChecked(false);
+            }
+
+        }
+    };
 }
